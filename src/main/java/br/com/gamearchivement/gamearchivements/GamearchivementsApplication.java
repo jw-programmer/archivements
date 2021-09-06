7a03382b05ae599c708d871241d1a78d3bc821e2
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2021-09-06 19:10:34 -0300

    Fist Commit

---
+++ src/main/java/br/com/gamearchivement/gamearchivements/GamearchivementsApplication.java
@@ -0,0 +1,13 @@
+package br.com.gamearchivement.gamearchivements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamearchivementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamearchivementsApplication.class, args);
+	}
+
+}
